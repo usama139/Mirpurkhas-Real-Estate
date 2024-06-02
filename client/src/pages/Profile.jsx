41aa5303fd0a779c4bfa22749f09225219dc35d1
usama <usamamaalik6@gmail.com>
2024-06-03 00:42:22 +0500

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -123,6 +123,23 @@ const handleShowListings = async () => {
   } catch (error) {
     setShowListingsError(true);
   }
+};
+
+const handleListingDelete = async (listingId) => {
+  try {
+    const res = await fetch(`/api/listing/delete/${listingId}`, {
+      method: 'DELETE',
+    });
+    const data = await res.json();
+    if(data.success === false){
+      console.log(data.message);
+      return;
+    }
+
+    setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+  } catch (error) {
+    console.log(error.message);
+  }
 }
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -192,7 +209,7 @@ const handleShowListings = async () => {
               <p >{listing.name}</p>
             </Link>
             <div className="flex flex-col items-center">
-                <button className="text-red-700 uppercase">Delete</button>
+                <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
                 <button className="text-green-700 uppercase">DEdit</button>
             </div>
           </div>
